954ef762c4562c809719398c4f109a2dee4f10c4
papilooz <manriquequervie@gmail.com>
2025-06-22 20:40:43 +0800

    refactor(php): update changes

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,15 +1,15 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php'; // Adjust the path if needed
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../'); // Adjust path to where .env is
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-$config = [
-    'pg_host' => $_ENV['PG_HOST'],
-    'pg_port' => $_ENV['PG_PORT'],
-    'pg_db'   => $_ENV['PG_DB'],
-    'pg_user' => $_ENV['PG_USER'],
-    'pg_pass' => $_ENV['PG_PASS'],
-    'mongo_uri' => $_ENV['MONGO_URI'],
-    'mongo_db'  => $_ENV['MONGO_DB'],
+return [
+    'pg_host'    => $_ENV['PG_HOST'],
+    'pg_port'    => $_ENV['PG_PORT'],
+    'pg_db'      => $_ENV['PG_DB'],
+    'pg_user'    => $_ENV['PG_USER'],
+    'pg_pass'    => $_ENV['PG_PASS'],
+    'mongo_uri'  => $_ENV['MONGO_URI'],
+    'mongo_db'   => $_ENV['MONGO_DB'],
 ];
